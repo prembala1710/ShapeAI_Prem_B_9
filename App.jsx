f8c8e76d63bcfe37c6da715c91f9d7d79a0c166c
prembala1710 <83074315+prembala1710@users.noreply.github.com>
2021-06-26 16:57:50 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Heading from "./Header.jsx";
+import Footer from "./Footer.jsx";
+import Note from "./Info.jsx";
+
+function App() {
+  return (
+    <div>
+      <Heading />
+      <Note />
+      <Note />
+      <Note />
+      <Note />
+      <Note />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
